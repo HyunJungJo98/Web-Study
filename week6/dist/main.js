6f15dbacfad3240371670e91798be5a99d5c8959
HyunJungJo98 <kchwoonn0827@naver.com>
2024-03-19 20:19:43 +0900

    [#10]Feat : 번들링 파일 생성

---
+++ week6/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.createElement("div");document.body.appendChild(e),e.innerHTML=(()=>{const e=new Date;return`${e.getHours()}:${e.getMinutes()}:${e.getSeconds()}`})()})();
