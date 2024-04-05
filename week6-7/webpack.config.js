2c4620f5fdcd90ad3edf2f2aef2dca6eb90d2fe7
HyunJungJo98 <kchwoonn0827@naver.com>
2024-04-05 15:31:40 +0900

    [#11]Feat : webpack-dev-server 실습

--- week6-7/webpack.config.js
+++ week6-7/webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     // output: './dist/bundle.js' 와 같음
     filename: 'main.js',
     path: path.resolve(__dirname + '/dist'),
   },
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 3000,
-    ignored: /node_modules/,
-    poll: true,
+  devServer: {
+    port: 8080,
+    hot: true,
+    compress: true,
+    devMiddleware: {
+      publicPath: '/dist',
+    },
+    static: {
+      directory: path.join(__dirname),
+      serveIndex: true,
+    },
   },
 };
