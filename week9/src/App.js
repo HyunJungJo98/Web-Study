7fef40fe3521806a51e4d99606e69bd6abf92889
조현정 <79002373+HyunJungJo98@users.noreply.github.com>
2024-12-28 15:50:04 +0900

    [#12]Feat : tanstack query 설치, 환경 설정

---
+++ week9/src/App.js
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App"></div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
